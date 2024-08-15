9039e7ae84a4b3e1911b735879b81e908866c81a
13Yogesh <yogeshroro1306@gmail.com>
2024-08-16 03:41:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+function Username()
+{
+
+    
+    let Uname=('Entri Elevate');
+    
+    if (Uname == Login)
+    {
+        alert('authentication success')
+        console.log("Authentication success")
+    }
+
+    else
+    {
+        console.log("Login Failed")
+    }
+
+}
+
+function Password()
+{
+    let Password=('admin123');
+    if (Password == Login)
+    {
+        console.log("Authentication success")
+    }
+
+    else
+    {
+        console.log("Login Failed")
+    }
+}
